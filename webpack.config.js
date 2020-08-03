d17f3d814cab48d084368589f4c8c8a23987d084
bbjansen <bobjansen@pm.me>
2020-08-03 16:29:44 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+module.exports = {
+  entry: "./app/index.js",
+  mode: "development",
+  plugins: [new CleanWebpackPlugin("build")],
+  output: {
+    filename: "index_combined.js",
+    libraryTarget: "umd",
+    path: path.resolve(__dirname, "bin")
+  },
+  devServer: {
+    contentBase: "./app",
+    publicPath: "/bin",
+    https: true,
+    port: 9025
+  }
+};
